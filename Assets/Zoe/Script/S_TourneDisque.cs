b4a5f67a9ca99380b172ca7026dd93fa72b2d11e
ZoeChrvx <zchervaux@artfx.fr>
2023-11-17 17:22:52 +0100

    [Code] Smooth Rotation

--- Assets/Zoe/Script/S_TourneDisque.cs
+++ Assets/Zoe/Script/S_TourneDisque.cs
@@ -1,35 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class S_TourneDisque : MonoBehaviour
 {
     //si true = gauche // si false = droite
     public bool turnToLeft;
+    public float speedRotation;
     public float rotationAngle;
+    
+    public bool isPressed;
+    private Vector3 rotation;
+    private Quaternion rot;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isPressed = false;
+        rotation = new Vector3(0, rotationAngle, 0);
+        rot = Quaternion.Euler(rotation.x, rotation.y, rotation.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isPressed) 
+        { 
+            if(turnToLeft)
+            {
+                rot = Quaternion.Euler(rotation.x, rotation.y, rotation.z);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rot, speedRotation * Time.deltaTime);
+            }
+            else
+            {
+                rot = Quaternion.Euler(rotation.x, 0, rotation.z);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rot, speedRotation * Time.deltaTime);
+            }
+        }
     }
+
+
 
     public void PushToTurn()
     {
         if (turnToLeft) 
         {
-            transform.Rotate(new Vector3(0, -rotationAngle, 0));
+            turnToLeft = false;
         }
-        else if (!turnToLeft)
+        else
         {
-            transform.Rotate(new Vector3(0,rotationAngle, 0));
-        }
+            turnToLeft = true;
+        }        
     }
-
 }
