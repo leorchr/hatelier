0344c3a2ea835662b6bcc38fc137fbe69870480c
vifclem <clement.expert@artfx.fr>
2023-11-17 17:06:52 +0100

    [fix] mold

--- Assets/GPP/Clement/Script/InteractibleObjects/S_Interact_mold.cs
+++ Assets/GPP/Clement/Script/InteractibleObjects/S_Interact_mold.cs
@@ -6,8 +6,6 @@ using UnityEngine.UI;
 
 public class S_Interact_mold : S_Interactable
 {
-    public static S_Interact_mold instance;
-
     [Header("Display Text")]
     public string description = "Press <color=red>RIGHT CLICK</color>";
     [Header("Inventory")]
@@ -16,20 +14,6 @@ public class S_Interact_mold : S_Interactable
 
     public S_Materials material;
     private bool isInInventory = false;
-
-    private void Awake()
-    {
-        if (!instance) instance = this;
-    }
-
-    private void Update()
-    {
-        if(isInInventory == true && (Input.GetMouseButtonDown(1)))
-        {
-            OnInventoryVisible();
-        }
-    }
-
     public override string GetDescription()
     {
         return description;
@@ -41,20 +25,19 @@ public class S_Interact_mold : S_Interactable
     }
     public override void Interact()
     {
-        isInInventory = true;
-        S_Player_Interaction.instance.OnInteraction();
-        mainInventory.gameObject.SetActive(true);
-        reducedInventory.gameObject.SetActive(false);
-        //display inventory ui
-    }
-
-    public void OnInventoryVisible()
-    {
-        if (isInInventory == true )
+        if (isInInventory == false)
+        {
+            mainInventory.gameObject.SetActive(true);
+            reducedInventory.gameObject.SetActive(false);
+            isInInventory = true;
+        }
+        else
         {
             mainInventory.gameObject.SetActive(false);
             reducedInventory.gameObject.SetActive(true);
             isInInventory = false;
         }
+       
     }
+    
 }
