0b78b00f452780f65243888c44597c17d9fee284
Alan Rostami <alan.rostami@artfx.fr>
2023-11-21 17:07:47 +0100

    Player Controller
    [modify] S_LeftStick

--- Assets/GPP/Alan/Scripts/S_LeftStick.cs
+++ Assets/GPP/Alan/Scripts/S_LeftStick.cs
@@ -5,7 +5,7 @@ public enum AxisOptions { Both, Horizontal, Vertical }
 
 public class S_LeftStick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
-     public float Horizontal { get { return (snapX) ? SnapFloat(input.x, AxisOptions.Horizontal) : input.x; } }
+    public float Horizontal { get { return (snapX) ? SnapFloat(input.x, AxisOptions.Horizontal) : input.x; } }
     public float Vertical { get { return (snapY) ? SnapFloat(input.y, AxisOptions.Vertical) : input.y; } }
     public Vector2 Direction { get { return new Vector2(Horizontal, Vertical); } }
 
