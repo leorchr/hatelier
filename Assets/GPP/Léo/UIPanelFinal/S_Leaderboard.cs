65876d99523777085523b0a1b5aa89d59a7def71
L3o <74620207+leorchr@users.noreply.github.com>
2023-11-22 18:04:04 +0100

    [code] ui final panel scripts and save system

---
+++ Assets/GPP/Léo/UIPanelFinal/S_Leaderboard.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class S_Leaderboard : MonoBehaviour
+{
+    public static S_Leaderboard instance;
+
+    [Min(0)] public int numberPlayersDisplay = 3;
+    [Space]
+    [SerializeField] private GameObject displayPrefab;
+    [SerializeField] private GameObject leaderboard;
+    [SerializeField] private GameObject inputField;
+    [SerializeField] private GameObject text;
+
+
+    private GameObject[] display;
+    private GameObject[] nameList;
+    private GameObject[] scoreList;
+
+    [HideInInspector] public int playerScoreTemp = 0;
+    [HideInInspector] public string playerNameTemp = "Name";
+
+    private void Awake()
+    {
+        if (!instance) instance = this;
+    }
+
+    private void Start()
+    {
+        display = new GameObject[numberPlayersDisplay];
+        nameList = new GameObject[numberPlayersDisplay];
+        scoreList = new GameObject[numberPlayersDisplay];
+
+        for (int i = 0; i < numberPlayersDisplay; i++)
+        {
+            display[i] = Instantiate(displayPrefab, leaderboard.transform);
+            nameList[i] = display[i].transform.GetChild(0).gameObject;
+            scoreList[i] = display[i].transform.GetChild(1).gameObject;
+        }
+    }
+
+    public void SetupLeaderboard()
+    {
+        playerNameTemp = text.GetComponent<TextMeshProUGUI>().text;
+        playerScoreTemp = Random.Range(0, 100); // récupérer le score du joueur ici
+        S_LeaderboardSave.instance.LoadFile();
+        S_LeaderboardSave.instance.SaveToFile();
+        S_LeaderboardSave.instance.saveData.UpdateUI();
+        leaderboard.SetActive(true);
+        inputField.SetActive(false);
+    }
+
+    public GameObject[] GetNameList() { return nameList; }
+    public GameObject[] GetScoreList() { return scoreList; }
+}
