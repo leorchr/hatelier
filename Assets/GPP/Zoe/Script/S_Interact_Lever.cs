4f98bb98a5ff126d022adfbd811e2b53c35e74e7
ZoeChrvx <zchervaux@artfx.fr>
2023-11-22 11:26:55 +0100

    [add] All Gpe on One Scene
    + [code] GPE Tourne Disque --> Script interact

---
+++ Assets/GPP/Zoe/Script/S_Interact_Lever.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class S_Interact_Lever : S_Interactable
+{
+
+    public string description = "Press <color=red>RIGHT CLICK</color>";
+    public string noMatDescription;
+    public S_TourneDisque tourneDisque;
+
+    public override string GetDescription()
+    {
+        if (S_Inventory.instance.GetMaterials() == null)
+        {
+            return description;
+        }
+        else
+        {
+            return description;
+        }
+    }
+
+    public override string GetMatDescription()
+    {
+        if (S_Inventory.instance.GetMaterials() == null)
+        {
+            return noMatDescription;
+        }
+        else
+        {
+            return noMatDescription;
+        }
+    }
+    public override void Interact()
+    {
+        if (S_Inventory.instance.GetMaterials() == null)
+        {
+            if (tourneDisque.turnToLeft)
+            {
+                tourneDisque.turnToLeft = false;
+            }
+            else
+            {
+                tourneDisque.turnToLeft = true;
+            }
+        }
+    }
+}
