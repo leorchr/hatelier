1797c0c1ea60604a14893d707f32a18974a624e0
ArkanioGX <86797763+ArkanioGX@users.noreply.github.com>
2023-11-27 15:19:39 +0100

    [Code] PlayerController and Animator redone

--- Assets/GPP/Alan/Scripts/S_PlayerController.cs
+++ Assets/GPP/Alan/Scripts/S_PlayerController.cs
@@ -8,7 +8,7 @@ public class S_PlayerController : MonoBehaviour
     public static S_PlayerController instance;
 
     Rigidbody m_rigidbody = null;
-    // Animator m_animator = null;
+    Animator m_animator = null;
     // Vector3 m_playerMoveInput  = Vector3.zero;
 
     [SerializeField] private S_LeftStick m_leftStick;
@@ -21,7 +21,7 @@ public class S_PlayerController : MonoBehaviour
     {
         if (!instance) instance = this;
         m_rigidbody = GetComponent<Rigidbody>();
-        // m_animator = GetComponent<Animator>();
+        m_animator = GetComponent<Animator>();
     }
 
     private bool isNotInMenu = true;
@@ -30,24 +30,31 @@ public class S_PlayerController : MonoBehaviour
     {
         Vector2 leftStick = new Vector2(m_leftStick.Horizontal, m_leftStick.Vertical );
         m_rigidbody.AddForce(leftStick.x * m_Acceleration , 0, leftStick.y * m_Acceleration , ForceMode.Acceleration) ;
-        Debug.Log(m_MaxSpeed + " | " + leftStick.x);
-        Debug.Log("Clamp X : " + (m_rigidbody.velocity.x) + " | " + (-m_MaxSpeed * Mathf.Abs(leftStick.x)) + " | " + (m_MaxSpeed * Mathf.Abs(leftStick.x)) );
+        //Debug.Log(m_MaxSpeed + " | " + leftStick.x);
+        //Debug.Log("Clamp X : " + (m_rigidbody.velocity.x) + " | " + (-m_MaxSpeed * Mathf.Abs(leftStick.x)) + " | " + (m_MaxSpeed * Mathf.Abs(leftStick.x)) );
+
+        //Add speed
         Vector2 maxSpd = new Vector2(
             m_MaxSpeed * Mathf.Max(m_decelerateRate, Mathf.Abs(leftStick.x)),
             m_MaxSpeed * Mathf.Max(m_decelerateRate, Mathf.Abs(leftStick.y)));
 
+        //Clamp speed
         m_rigidbody.velocity = new Vector3(
             Mathf.Clamp(m_rigidbody.velocity.x,-maxSpd.x, maxSpd.x), 
             m_rigidbody.velocity.y,
             Mathf.Clamp(m_rigidbody.velocity.z, -maxSpd.y, maxSpd.y));
 
-
+        //Rotation of the player
         if (m_leftStick.Horizontal != 0 || m_leftStick.Vertical != 0)
         {
             float yRotation = Quaternion.LookRotation(m_rigidbody.velocity).eulerAngles.y;
             transform.rotation = Quaternion.Euler(transform.rotation.x,yRotation,transform.rotation.z) ;
         }
 
+        float t = m_rigidbody.velocity.magnitude;
+
+        Debug.Log(m_rigidbody.velocity.sqrMagnitude);
+        m_animator.SetBool("isMoving", m_rigidbody.velocity.sqrMagnitude > 0.2f);
         // m_animator.SetFloat("Horizontal", m_leftStick.Horizontal);
         // m_animator.SetFloat("Vertical", m_leftStick.Vertical);
     }
