13a4f25816e65e51c80d0db9cf240af62f5441d7
ArkanioGX <86797763+ArkanioGX@users.noreply.github.com>
2023-11-29 17:07:11 +0100

    [CODE] added a window to mody every SH_MAIN material

---
+++ Assets/GPP/Clarence/Scripts/S_ChangeMaterial.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class S_ChangeMaterial : EditorWindow
+{
+    ColorField shadowColorField;
+    ColorField lightColorField;
+    /// Add a new menu item under an existing menu
+
+    [MenuItem("Tools/Change SH_Main Material Value")]
+    public static void Initialize()
+    {
+        // This method is called when the user selects the menu item in the Editor
+        EditorWindow wnd = GetWindow<S_ChangeMaterial>();
+        wnd.titleContent = new GUIContent("Change Material Value");
+
+        wnd.minSize = new Vector2(200,100);
+    }
+
+    Shader shaderBase ;
+    private List<Material> materials = new();
+
+    private void ApplyChange()
+    {
+
+        shaderBase = Shader.Find("Shader Graphs/SH_MAIN_SHADER");
+        if (shaderBase != null)
+        {
+            
+            int count = 0;
+            materials.Clear();
+            string[] allMaterialPaths = AssetDatabase.FindAssets("t:Material");
+            foreach (string path in allMaterialPaths)
+            {
+                string assetPath = AssetDatabase.GUIDToAssetPath(path);
+                if (assetPath.StartsWith("Assets/"))
+                {
+                    Material mat = AssetDatabase.LoadAssetAtPath<Material>(assetPath);
+                    if (mat != null && mat.shader != null && !string.IsNullOrEmpty(mat.shader.name))
+                    {
+                        
+                        if (mat.shader.name.Equals(shaderBase.name, StringComparison.OrdinalIgnoreCase))
+                        {
+                            materials.Add(mat);
+                            count++;
+                        }
+                    }
+                }
+
+            }
+            materials.Sort((a, b) => a.name.CompareTo(b.name));
+        }
+        
+        foreach(Material mat in materials)
+        {
+            mat.SetColor("_Light_Color", lightColorField.value);
+            mat.SetColor("_Shadows_Color", shadowColorField.value);
+        }
+    }
+
+    
+
+    private void CreateGUI()
+    {
+        VisualElement root = rootVisualElement;
+
+        shadowColorField = new ColorField();
+        shadowColorField.label = "Shadow Color";
+
+        lightColorField = new ColorField();
+        lightColorField.label = "Light Color";
+
+        Button button = new Button(ApplyChange);
+        button.text = "Apply to all material";
+        root.Add(shadowColorField);
+        
+        root.Add(lightColorField);
+
+        root.Add(button);
+
+    }
+}
