41cf3d94013be62659256df06e08ae7ecb4d484c
vifclem <clement.expert@artfx.fr>
2023-11-30 12:00:22 +0100

    [add] - UI craft inventory
    [CODE]  Changed mold inventory and interact mold so the mold ui are independent

--- Assets/GPP/Clement/Script/InteractibleObjects/S_Interact_mold.cs
+++ Assets/GPP/Clement/Script/InteractibleObjects/S_Interact_mold.cs
@@ -11,19 +11,16 @@ public class S_Interact_mold : S_Interactable
     public string cannotUseMatText = "<color=red>CANNOT PUT 2 SAME MATERIALS</color>";
     public string inventoryEmpty = "<color=red>CANNOT USE THE MOLD IF NO MATERIAL IN YOUR INVENTORY</color>";
     [Header("Inventory")]
-    public Image reducedInventory;
+    private Image reducedInventory;
     public GameObject mainInventoryGroup;
-
-    private Button BakeButton;
 
     private S_Mold_Inventory moldInventory;
 
     public S_Materials mold;
-    private bool isInInventory = false;
 
     private void Start()
     {
-        BakeButton = mainInventoryGroup.transform.Find("Bake").GetComponent<Button>();
+        reducedInventory = S_UI_Inventory.instance.GetComponent<Image>();
         moldInventory = GetComponent<S_Mold_Inventory>();
     }
 
@@ -68,16 +65,10 @@ public class S_Interact_mold : S_Interactable
     public override void Interact()
     {
         
-        if (S_Inventory.instance.GetMaterials() != moldInventory.GetMaterial1() || isInInventory == true)
+        if (S_Inventory.instance.GetMaterials() != moldInventory.GetMaterial1())
         {
 
-            if (isInInventory == false)
-            {
-                S_Menu_Manager.Instance.stopPlayer(false);
-                mainInventoryGroup.gameObject.SetActive(true);
-                reducedInventory.gameObject.SetActive(false);
-                S_UI_Inventory.instance.SetMoldInventory(moldInventory);
-                S_UI_Inventory.instance.refreshMoldInv();
+                moldInventory.refreshMoldInv();
 
                 if(S_Inventory.instance.GetMaterials() != null)
                 if (!moldInventory.IsInventoryFull() && S_Inventory.instance.GetMaterials().canBeBaked)
@@ -86,28 +77,32 @@ public class S_Interact_mold : S_Interactable
                     moldInventory.AddToInventory(S_Inventory.instance.GetMaterials());
                     S_Inventory.instance.ClearInventory();
                     S_UI_Inventory.instance.ClearPlayerInventoryIcon();
-                    BakeButton.onClick.AddListener(PressedBakeButton);
+                    
                 }
 
                 // Quand le ui sera terminer on fera comme ca au lieu du boutton bake
-                //if (moldInventory.IsInventoryFull())
-                //{
-                //    moldInventory.CheckRecipeMaterialWithMoldMaterial();
-                //}
-               
-                isInInventory = true;
-            }
-            else
-            {
-                S_Menu_Manager.Instance.stopPlayer(true);
-                mainInventoryGroup.gameObject.SetActive(false);
-                reducedInventory.gameObject.SetActive(true);
-                new WaitForSeconds(3);
-                //S_UI_Inventory.instance.ClearMoldInventoryStatueIcon();
-                isInInventory = false;
-                BakeButton.onClick.RemoveAllListeners();
+                if (moldInventory.IsInventoryFull())
+                {
+                    moldInventory.CheckRecipeMaterialWithMoldMaterial();
 
-            }
+
+                    moldInventory.ClearMoldInventoryStatueIcon();
+                    moldInventory.ClearStatueSlot();
+
+                 }
+               
+                //isInInventory = true;
+            //}
+            //else
+            //{
+               // S_Menu_Manager.Instance.stopPlayer(true);
+                //mainInventoryGroup.gameObject.SetActive(false);
+                //reducedInventory.gameObject.SetActive(true);
+                //S_UI_Inventory.instance.ClearMoldInventoryStatueIcon();
+                //isInInventory = false;
+                //BakeButton.onClick.RemoveAllListeners();
+
+            //}
         }
        
         
@@ -116,13 +111,5 @@ public class S_Interact_mold : S_Interactable
 
     }
 
-    public void PressedBakeButton()
-    {
-        //Lunch timer 
-        //add statue to player inventory
-        moldInventory.CheckRecipeMaterialWithMoldMaterial();
-        Debug.Log("TEST" + gameObject.name);
-        
-    }
     
 }
