c78465c6a8ad9edbc1467dfa3f6a39613245d6d8
ArkanioGX <86797763+ArkanioGX@users.noreply.github.com>
2023-12-06 11:18:28 +0100

    [Code] Dust vfx played when player make a step

---
+++ Assets/S_Player_Anim_Manager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class S_Player_Anim_Manager : MonoBehaviour
+{
+    private Animator c_Animator;
+
+    public VisualEffect DustVFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        c_Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void walkDust()
+    {
+        Vector3 footPos = Vector3.zero;
+        
+        DustVFX.SetVector3("Foot_Location", footPos);
+        DustVFX.Play();
+    }
+}
