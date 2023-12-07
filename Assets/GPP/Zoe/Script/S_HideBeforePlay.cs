96fc2229ba48827512055f36f9de26778e7eb199
ZoeChrvx <zchervaux@artfx.fr>
2023-12-07 14:46:01 +0100

    [fix] Alpha ui

--- Assets/GPP/Zoe/Script/S_HideBeforePlay.cs
+++ Assets/GPP/Zoe/Script/S_HideBeforePlay.cs
@@ -8,29 +8,18 @@ public class S_HideBeforePlay : MonoBehaviour
 {
     public static S_HideBeforePlay instance;
 
-    [Header("Left Stick")]
+    [SerializeField] private List<GameObject> uiElements;
+
+    [SerializeField] private float colorAlphaLow;
+    [SerializeField] private float colorAlphaHigh;
+
+    [Header("GameObject with functions")]
     [SerializeField] private GameObject leftStick;
-    [SerializeField] private GameObject handleLS;
-    [Header("Interact Button")]
     [SerializeField] private GameObject interactButton;
-    [SerializeField] private GameObject imageIB;
-    [Header("Pause Button")]
-    [SerializeField] private GameObject pauseButtton;
-    [Header("Inventory (Barre)")]
-    //[SerializeField] private GameObject inventoryComposant0;
-    //[SerializeField] private GameObject inventoryComposant1;
-    //[SerializeField] private GameObject inventoryComposant2;
-    //[SerializeField] private GameObject inventoryComposant3;
-    //[SerializeField] private GameObject inventoryComposant4;
-    //[SerializeField] private GameObject inventoryComposant5;
-    //[SerializeField] private GameObject inventoryComposant6;
-    [Header("Inventory + drop Button")]
-    //[SerializeField] private GameObject inventoryComposant7;
-    //[SerializeField] private GameObject inventoryComposant8;
+    [SerializeField] private GameObject pauseButton;
     [SerializeField] private GameObject dropButton;
 
-    public Color colorAlphaLow;
-    public Color colorAlphaHigh;
+    [TextArea][Tooltip("Ok")] public string noteList;
 
     private void Awake()
     {
@@ -41,49 +30,32 @@ public class S_HideBeforePlay : MonoBehaviour
     {
         if (!S_WarmUpTimer.instance.gameBegin)
         {
+            foreach (var elements in uiElements)
+            {
+                Color elementsColor = elements.GetComponent<Image>().color;
+                elementsColor = new Color(elementsColor.r, elementsColor.g, elementsColor.b, colorAlphaLow);
+                elements.GetComponent<Image>().color = elementsColor;
+                Debug.Log("cheh");
+            }
+
             leftStick.GetComponent<S_LeftStick>().enabled = false;
             interactButton.GetComponent<Button>().enabled = false;
-            pauseButtton.GetComponent<Button>().enabled = false;
+            pauseButton.GetComponent<Button>().enabled = false;
             dropButton.GetComponent<Button>().enabled = false;
-            //Image Alpha
-            leftStick.GetComponent<Image>().color = colorAlphaLow;
-            handleLS.GetComponent<Image>().color = colorAlphaLow;
-            interactButton.GetComponent<Image>().color = colorAlphaLow;
-            imageIB.GetComponent<Image>().color = colorAlphaLow;
-            pauseButtton.GetComponent<Image>().color = colorAlphaLow;
-            //inventoryComposant0.GetComponent<Image>().color = colorAlphaLow;
-            //inventoryComposant1.GetComponent<Image>().color = colorAlphaLow;
-            //inventoryComposant2.GetComponent<Image>().color = colorAlphaLow;
-            //inventoryComposant3.GetComponent<Image>().color = colorAlphaLow;
-            //inventoryComposant4.GetComponent<Image>().color = colorAlphaLow;
-            //inventoryComposant5.GetComponent<Image>().color = colorAlphaLow;
-            //inventoryComposant6.GetComponent<Image>().color = colorAlphaLow;
-            //inventoryComposant7.GetComponent<Image>().color = colorAlphaLow;
-            //inventoryComposant8.GetComponent<Image>().color = colorAlphaLow;
-            //dropButton.GetComponent<Image>().color = colorAlphaLow;
         }
         else
         {
+            foreach (var elements in uiElements)
+            {
+                Color elementsColor = elements.GetComponent<Image>().color;
+                elementsColor = new Color(elementsColor.r, elementsColor.g, elementsColor.b, colorAlphaHigh);
+                elements.GetComponent<Image>().color = elementsColor;
+            }
+
             leftStick.GetComponent<S_LeftStick>().enabled = true;
             interactButton.GetComponent<Button>().enabled = true;
-            pauseButtton.GetComponent<Button>().enabled= true;
+            pauseButton.GetComponent<Button>().enabled= true;
             dropButton.GetComponent<Button>().enabled= true;
-            //Image Alpha
-            leftStick.GetComponent<Image>().color = colorAlphaHigh;
-            handleLS.GetComponent<Image>().color = colorAlphaHigh;
-            interactButton.GetComponent<Image>().color = colorAlphaHigh;
-            imageIB.GetComponent<Image>().color = colorAlphaHigh;
-            pauseButtton.GetComponent<Image>().color = colorAlphaHigh;
-            //inventoryComposant0.GetComponent<Image>().color = colorAlphaHigh;
-            //inventoryComposant1.GetComponent<Image>().color = colorAlphaHigh;
-            //inventoryComposant2.GetComponent<Image>().color = colorAlphaHigh;
-            //inventoryComposant3.GetComponent<Image>().color = colorAlphaHigh;
-            //inventoryComposant4.GetComponent<Image>().color = colorAlphaHigh;
-            //inventoryComposant5.GetComponent<Image>().color = colorAlphaHigh;
-            //inventoryComposant6.GetComponent<Image>().color = colorAlphaHigh;
-            //inventoryComposant7.GetComponent<Image>().color = colorAlphaHigh;
-            //inventoryComposant8.GetComponent<Image>().color = colorAlphaHigh;
-            //dropButton.GetComponent<Image>().color = colorAlphaHigh;
         }
     }
 }
