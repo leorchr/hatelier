b6233deff5274eb4ab09548fbfedbffaa833389d
Komediem <gbussola@artfx.fr>
2023-12-11 16:09:14 +0100

    [dev] - [add] UI and Material

--- Assets/GPP/Clement/Script/S_InstantiateStatue.cs
+++ Assets/GPP/Clement/Script/S_InstantiateStatue.cs
@@ -8,7 +8,13 @@ using UnityEngine;
 public class S_InstantiateStatue : MonoBehaviour
 {
     public static S_InstantiateStatue instance;
-    
+
+    [SerializeField] private Texture2D baseColor1;
+    [SerializeField] private Texture2D normal1;
+
+    [SerializeField] private Texture2D baseColor2;
+    [SerializeField] private Texture2D normal2;
+
     private void Awake()
     {
         instance = this;
@@ -16,13 +22,19 @@ public class S_InstantiateStatue : MonoBehaviour
     public void AttributeSpecs(GameObject statue)
     {
         statue.GetComponent<MeshRenderer>().material = S_Statue_Inventory.instance.top.materials;
-        
+
+        statue.GetComponent<MeshRenderer>().material.SetTexture("_Layer1_01", baseColor1);
+        statue.GetComponent<MeshRenderer>().material.SetTexture("_Layer1_01_Normal", normal1);
+
+        statue.GetComponent<MeshRenderer>().material.SetTexture("_Layer2_01", baseColor1);
+        statue.GetComponent<MeshRenderer>().material.SetTexture("_Layer2_02_Normal", baseColor1);
+
         //couleur
-        
+
     }
 
     public void InstantiateStatue(GameObject statue)
     {
-       
+        Instantiate(statue);
     }
 }
