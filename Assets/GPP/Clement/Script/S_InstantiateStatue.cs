1cbdb67aabd881a866afecc9fc4f250b29b342ba
vifclem <clement.expert@artfx.fr>
2023-12-12 10:25:29 +0100

    [fix]

--- Assets/GPP/Clement/Script/S_InstantiateStatue.cs
+++ Assets/GPP/Clement/Script/S_InstantiateStatue.cs
@@ -1,8 +1,3 @@
-using JetBrains.Annotations;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class S_InstantiateStatue : MonoBehaviour
