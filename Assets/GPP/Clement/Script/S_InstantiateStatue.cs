97acb7df5b9bfcc1fe619525f58c268b832e8a5c
vifclem <clement.expert@artfx.fr>
2023-12-12 12:23:52 +0100

    [add] Instantiate statue

--- Assets/GPP/Clement/Script/S_InstantiateStatue.cs
+++ Assets/GPP/Clement/Script/S_InstantiateStatue.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class S_InstantiateStatue : MonoBehaviour
 {
     public static S_InstantiateStatue instance;
-
+    private GameObject finalStatue;
   
 
     private void Awake()
@@ -17,12 +17,9 @@ public class S_InstantiateStatue : MonoBehaviour
         statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_01", S_Statue_Inventory.instance.bottom.baseColor1);
         statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_01_Normal", S_Statue_Inventory.instance.bottom.normal1);
 
-        //statue.GetComponent<MeshRenderer>().material.SetTexture("_Layer2_01", S_Statue_Inventory.instance.bottom.baseColor2);
-        //statue.GetComponent<MeshRenderer>().material.SetTexture("_Layer2_02_Normal", S_Statue_Inventory.instance.bottom.normal2);
+        finalStatue = Instantiate(statue, gameObject.transform);
+        finalStatue.AddComponent<S_SculptRotate>();
 
-        Instantiate(statue, gameObject.transform);
-        statue.AddComponent<S_SculptRotate>();
-        //add component, script de clarence bouger la statue
 
     }
 
