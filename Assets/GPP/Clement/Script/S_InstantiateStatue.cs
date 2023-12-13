f339fe01523a9a29d2a16b77a17c72f2b2c8124d
vifclem <clement.expert@artfx.fr>
2023-12-13 14:56:30 +0100

    [add] Reset Texture systeme

--- Assets/GPP/Clement/Script/S_InstantiateStatue.cs
+++ Assets/GPP/Clement/Script/S_InstantiateStatue.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class S_InstantiateStatue : MonoBehaviour
 {
     public static S_InstantiateStatue instance;
+    public Texture2D defaultTexture;
+    public List<GameObject> statues = new List<GameObject>();
   
 
     private void Awake()
@@ -11,18 +14,40 @@ public class S_InstantiateStatue : MonoBehaviour
     }
     public void AttributeSpecs(GameObject statue)
     {
-        if(S_Statue_Inventory.instance.top != null) statue.GetComponent<MeshRenderer>().sharedMaterial = S_Statue_Inventory.instance.top.materials;
+        
+        if(S_Statue_Inventory.instance.top != null)
+        {
+            statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_01", S_Statue_Inventory.instance.top.baseColor1);
+            statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_01_Normal", S_Statue_Inventory.instance.top.normal1);
+        }
 
         if (S_Statue_Inventory.instance.bottom != null)
         {
-            statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_01", S_Statue_Inventory.instance.bottom.baseColor1);
-            statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_01_Normal", S_Statue_Inventory.instance.bottom.normal1);
-
+           
             statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_02", S_Statue_Inventory.instance.bottom.baseColor2);
             statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_02_Normal", S_Statue_Inventory.instance.bottom.normal2);
+
+            statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer2_01", S_Statue_Inventory.instance.bottom.baseColor3);
+            statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer2_01_Normal", S_Statue_Inventory.instance.bottom.normal3);
         }
 
         GameMode.instance.finalStatue = statue;
         
+    }
+
+    public void ResetText()
+    {
+        foreach (var item in statues)
+        {
+            item.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_01", null);
+            item.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_01_Normal", null);
+
+            item.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_02", null);
+            item.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_02_Normal", null);
+
+            item.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer2_01", null);
+            item.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer2_01_Normal", null);
+
+        }
     }
 }
