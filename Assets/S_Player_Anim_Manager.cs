e48dd2da5e53fee1b40dacfe46561db7ca95cde3
ArkanioGX <86797763+ArkanioGX@users.noreply.github.com>
2023-12-13 12:29:14 +0100

    [Code] Animation Done

--- Assets/S_Player_Anim_Manager.cs
+++ Assets/S_Player_Anim_Manager.cs
@@ -1,24 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.VFX;
 
 public class S_Player_Anim_Manager : MonoBehaviour
 {
-    private Animator c_Animator;
+    private Animator m_Animator;
+    private Rigidbody m_Rigidbody;
+
+    public float minSpeedToWalk = 0.2f;
+
+    private float maxSpeed;
 
     public VisualEffect DustVFX;
 
     // Start is called before the first frame update
     void Start()
     {
-        c_Animator = GetComponent<Animator>();
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+        maxSpeed = GetComponent<S_PlayerController>().currentMaxSpeed();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        m_Animator.SetInteger("Speed",Convert.ToInt16(m_Rigidbody.velocity.sqrMagnitude > minSpeedToWalk));
+    }
+
+    public void disablePickUp()
+    {
+        m_Animator.SetBool("PickUP", false);
     }
 
     public void walkDust()
@@ -30,4 +43,41 @@ public class S_Player_Anim_Manager : MonoBehaviour
 
         S_PlayerSound.instance.StepSound();
     }
+
+    public void setPushing(bool b)
+    {
+        m_Animator.SetBool("IsPushing", b);
+    }
+
+    public void setPickUp(bool b)
+    {
+        m_Animator.SetBool("PickUP", b);
+    }
+
+    
+
+    public void setPushingSpeed(dir d)
+    {
+        float f = Mathf.Clamp(Mathf.InverseLerp(0, maxSpeed, m_Rigidbody.velocity.sqrMagnitude), 0,1f) ;
+        Vector3 v = m_Rigidbody.velocity;
+        switch (d)
+        {
+            case dir.Left:
+                if (v.x < 0) { f *= -1; }
+                break;
+            case dir.Right:
+                if (v.x > 0) { f *= -1; }
+                break;
+            case dir.Front:
+                if (v.z > 0) { f *= -1; }
+                break;
+            case dir.Back:
+                if (v.z < 0) { f *= -1; }
+                break;
+            default: break;
+        }
+        m_Animator.SetFloat("PushPullSpeed", f);
+    }
+
+    
 }
