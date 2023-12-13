6edf41b1fbb70368de18c1aa802a91a93ca4ff06
ArkanioGX <clarencemollet1@gmail.com>
2023-12-13 23:20:16 +0100

    [code] walk dust fix

---
+++ Assets/walkDustAdapter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walkDustAdapter : MonoBehaviour
+{
+    S_Player_Anim_Manager anim;
+
+    private void Awake()
+    {
+        anim = transform.parent.GetComponent<S_Player_Anim_Manager>();
+    }
+    public void WalkDust()
+    {
+        anim.walkDust();
+    }
+}
