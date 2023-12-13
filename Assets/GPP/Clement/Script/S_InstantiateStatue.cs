794974b190fb3930868bf2618aae4d871e1a0e94
L3o <leogravillon@hotmail.fr>
2023-12-13 11:51:22 +0100

    [fix] statue display endgame
    [dev] statue scriptable objects and prefabs

--- Assets/GPP/Clement/Script/S_InstantiateStatue.cs
+++ Assets/GPP/Clement/Script/S_InstantiateStatue.cs
@@ -11,16 +11,18 @@ public class S_InstantiateStatue : MonoBehaviour
     }
     public void AttributeSpecs(GameObject statue)
     {
-        statue.GetComponent<MeshRenderer>().sharedMaterial = S_Statue_Inventory.instance.top.materials;
+        if(S_Statue_Inventory.instance.top != null) statue.GetComponent<MeshRenderer>().sharedMaterial = S_Statue_Inventory.instance.top.materials;
 
-        statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_01", S_Statue_Inventory.instance.bottom.baseColor1);
-        statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_01_Normal", S_Statue_Inventory.instance.bottom.normal1);
+        if (S_Statue_Inventory.instance.bottom != null)
+        {
+            statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_01", S_Statue_Inventory.instance.bottom.baseColor1);
+            statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_01_Normal", S_Statue_Inventory.instance.bottom.normal1);
 
-        statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_02", S_Statue_Inventory.instance.bottom.baseColor2);
-        statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_02_Normal", S_Statue_Inventory.instance.bottom.normal2);
+            statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_02", S_Statue_Inventory.instance.bottom.baseColor2);
+            statue.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_Layer1_02_Normal", S_Statue_Inventory.instance.bottom.normal2);
+        }
 
         GameMode.instance.finalStatue = statue;
         GameMode.instance.finalStatue.AddComponent<S_SculptRotate>();
     }
-
 }
