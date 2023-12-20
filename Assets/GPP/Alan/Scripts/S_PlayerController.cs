5a387bcf191461752ab54558080cbcbd5c6d9827
ArkanioGX <86797763+ArkanioGX@users.noreply.github.com>
2023-12-20 15:06:24 +0100

    true fix push collider

--- Assets/GPP/Alan/Scripts/S_PlayerController.cs
+++ Assets/GPP/Alan/Scripts/S_PlayerController.cs
@@ -169,13 +169,15 @@ public class S_PlayerController : MonoBehaviour
 
 
             Debug.Log(fwd);
-            if (fwd.x == 0)
+            if (Mathf.Abs(fwd.x) < Mathf.Abs(fwd.z))
             {
                 m_PushCollider.size = new Vector3(scale.x * bc.size.x, scale.y * bc.size.y, scale.z * bc.size.z);
+                Debug.Log("x 0");
             }
             else
             {
                 m_PushCollider.size = new Vector3(scale.z * bc.size.z, scale.y * bc.size.y, scale.x * bc.size.x );
+                Debug.Log("z 0");
             }
 
             float gap = Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(go.transform.position.x, go.transform.position.z)) ;
