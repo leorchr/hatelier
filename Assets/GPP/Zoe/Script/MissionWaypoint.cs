86a315469eb88ce4e1a9346c0c4e5a503a4f36b6
ZoeChrvx <zchervaux@artfx.fr>
2023-12-21 16:17:21 +0100

    [add] Waypoint !

---
+++ Assets/GPP/Zoe/Script/MissionWaypoint.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionWaypoint : MonoBehaviour
+{
+    public Image img;
+    public Transform target;
+    public Vector3 offset;
+    public List<GameObject> etabli;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameMode.instance.currentPhase == 1)
+        {
+            target = etabli[0].transform;
+        }
+        else if(GameMode.instance.currentPhase == 2)
+        {
+            target = etabli[1].transform;
+        }
+        else if (GameMode.instance.currentPhase == 3)
+        {
+            target = etabli[2].transform;
+        }
+        float minX = img.GetPixelAdjustedRect().width / 2;
+        float maxX = Screen.width - minX;
+
+        float minY = img.GetPixelAdjustedRect().height / 2;
+        float maxY = Screen.height - minY;
+
+        Vector2 pos = Camera.main.WorldToScreenPoint(target.position + offset);
+
+        /*if(Vector3.Dot((target.position - transform.position), transform.forward) < 0)
+        {
+            if(pos.x < Screen.width / 2)
+            {
+                pos.x = maxX;
+            }
+            else
+            {
+                pos.x = minX;
+            }
+            
+        }*/
+
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        img.transform.position = pos;
+    }
+}
